250d0be3fa4a5efc38dabe9abe4a814836a39c9b
Indigo_cE9 <chrisyeohc@outlook.com>
2025-04-08 21:17:03 +0800

    add s3 bucket

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "sctp-ce9-tfstate"
+    key    = "chrisy-s3-tf-ci.tfstate"
+    region = "us-east-1"
+  }
+  required_version = ">= 1.0"
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+}
