09c04cadae2916dab554e191f52d97cedcd79df6
Indigo_cE9 <chrisyeohc@outlook.com>
2025-04-08 21:23:18 +0800

    resolve error in checov skips

--- main.tf
+++ main.tf
@@ -26,10 +26,6 @@ locals {
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
-}
-
-resource "aws_s3_bucket" "s3_tf" {
   # checkov:skip=CKV2_AWS_62:Event notifications not required for this bucket
   # checkov:skip=CKV_AWS_145:Using default AWS S3 encryption (SSE-S3) instead of KMS
   # checkov:skip=CKV_AWS_21:Versioning not enabled for cost optimization
